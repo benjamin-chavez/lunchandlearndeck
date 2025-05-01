479fc042a7722685e30c5e753540e7a294fc6de2
Ben Chavez <ben.m.chavez@gmail.com>
2025-05-01 01:21:29 -0500

    updated images

--- src/app/_data.ts
+++ src/app/_data.ts
@@ -5,12 +5,12 @@ export const slidesData = [
       "Save and see your changes instantly.",
       "What is wrong with this code?",
     ],
-    tabImages: ["1.1.svg", "1.2.svg"],
+    tabImages: ["img-0.0.png", "img-0.1.png",'img-0.2.png','img-0.3.png'],
   },
   {
     title: "Static Type Checking",
     bullets: ["Catch errors before runtime.", "Improve code maintainability."],
-    tabImages: ["image-1.0.svg", "image-1.1.svg"],
+    tabImages: ["img-1.0.png", "img-1.1.png",'img-1.2.png','img-1.3.png'],
   },
   {
     title: "Type Inference",

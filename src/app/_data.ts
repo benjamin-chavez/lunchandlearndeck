412498e38731f99c810749e8de75d9e1c8e6e69f
Ben Chavez <ben.m.chavez@gmail.com>
2025-05-01 13:16:04 -0500

    update

--- src/app/_data.ts
+++ src/app/_data.ts
@@ -43,7 +43,7 @@ export const slidesData = [
       "Runtime type checking vs mypy's static analysis",
       "Complementary tools",
     ],
-    tabImages: [],
+    tabImages: ["pydantic.png"],
     slideImg: [],
   },
   {

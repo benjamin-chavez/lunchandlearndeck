cff08e708e75c08461bb437b763bc3d37ab39a5a
Ben Chavez <ben.m.chavez@gmail.com>
2025-05-01 10:19:35 -0500

    updatE

--- src/app/_data.ts
+++ src/app/_data.ts
@@ -6,8 +6,8 @@ export const slidesData = [
       "Static Typing through the lens of dynamic types",
       "Trade-offs? - Development Speed, bug quantity",
     ],
-    tabImages: [],
-    slideImg: ["/mypy_light_logo.svg"],
+    tabImages: ['mypy_light_logo.svg', '2.0.png'],
+    slideImg: [""],
   },
   {
     // https://youtu.be/kSXZHRsWXfU?t=635
